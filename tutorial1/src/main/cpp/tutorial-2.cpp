e6294978e97736e70a8943188e243ffedc4900e6
Joe Chan <1030310877@qq.com>
2017-11-03 11:53:06 +0800

    Add tutorial2

--- tutorial1/src/main/cpp/tutorial-2.cpp
+++ tutorial1/src/main/cpp/tutorial-2.cpp
@@ -3,15 +3,25 @@
 //
 
 #include <jni.h>
+#include "clog.h"
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_joe_com_ndktutorial_activity_Tutorial2Activity_callMethod(JNIEnv *env, jobject instance) {
-
+    jclass activity = env->GetObjectClass(instance);
+    jmethodID mid = env->GetMethodID(activity, "changeText", "()V");
+    if (mid != NULL) {
+        env->CallVoidMethod(instance, mid, NULL);
+    } else { LOGE("can't find method"); }
 }
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_joe_com_ndktutorial_activity_Tutorial2Activity_callStaticMethod(JNIEnv *env, jobject instance) {
-
+Java_joe_com_ndktutorial_activity_Tutorial2Activity_callStaticMethod(JNIEnv *env,
+                                                                     jobject instance) {
+    jclass activity = env->FindClass("joe/com/ndktutorial/activity/Tutorial2Activity");
+    jmethodID mid = env->GetStaticMethodID(activity, "changeStaticText", "()V");
+    if (mid != NULL) {
+        env->CallStaticVoidMethod(activity, mid, NULL);
+    } else { LOGE("can't find method"); }
 }
--- tutorial1/src/main/java/joe/com/ndktutorial/activity/Tutorial2Activity.java
+++ tutorial1/src/main/java/joe/com/ndktutorial/activity/Tutorial2Activity.java
@@ -3,6 +3,8 @@ package joe.com.ndktutorial.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import joe.com.ndktutorial.R;
 
@@ -11,18 +13,39 @@ import joe.com.ndktutorial.R;
  * Created by joe on 2017/11/1.
  */
 public class Tutorial2Activity extends AppCompatActivity {
+    private static TextView textView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tutorial_2);
+        textView = (TextView) findViewById(R.id.tv_tutorial_2);
+        findViewById(R.id.btn_tutorial_2_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callMethod();
+            }
+        });
+        findViewById(R.id.btn_tutorial_2_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callStaticMethod();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textView = null;
     }
 
     private void changeText() {
-
+        textView.setText("JNI调用了非静态方法");
     }
 
     private static void changeStaticText() {
-
+        textView.setText("JNI调用了静态方法");
     }
 
     static {
